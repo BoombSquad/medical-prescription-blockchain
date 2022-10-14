00e024b6fb920f091b6e107d802ed25395f1a601
unknown <appelpedroappel@gmail.com>
2022-10-14 10:40:56 -0300

    fix: removing cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Our app is running on port ${PORT}`);
